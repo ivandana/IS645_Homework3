37cea16541cec1d4ea8937fb06877d486c27e9f4
ivandana <70627290+ivandana@users.noreply.github.com>
2021-02-09 00:01:52 -0800

    Updated

--- js/ex2.js
+++ js/ex2.js
@@ -9,31 +9,31 @@ var sat="Saturday";
 
 switch (day) {
   case "sun":
-    console.log("You entered: "+ sun)
+    console.log("You entered: "+ sun);
     console.log("The following day is: "+ mon);
     break;
   case "mon":
-    console.log("You entered: "+ mon)
+    console.log("You entered: "+ mon);
     console.log("The following day is: "+ tue);
     break;
   case "tue":
-    console.log("You entered: "+ tue)
+    console.log("You entered: "+ tue);
     console.log("The following day is: "+ wed);
     break;  
   case "wed":
-    console.log("You entered: "+ wed)
+    console.log("You entered: "+ wed);
     console.log("The following day is: "+ thu);
     break;
   case "thu":
-    console.log("You entered: "+ thu)
+    console.log("You entered: "+ thu);
     console.log("The following day is: "+ fri);  
     break;
   case "fri":
-    console.log("You entered: "+ fri)
+    console.log("You entered: "+ fri);
     console.log("The following day is: "+ sat);  
     break;
   case "sat":
-    console.log("You entered: "+ sat)
+    console.log("You entered: "+ sat);
     console.log("The following day is: "+ sun);
   default:
     console.log("Not a valid day type");
